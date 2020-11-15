94825f00534f778e96cf93a7a5a11903d0e06faa
cheskel <twerskycheskel@gmail.com>
2020-11-15 18:41:04 +0200

    fixed setup file

--- setup.sh
+++ setup.sh
@@ -4,7 +4,12 @@
 pip3 install -r requirements.txt
 sudo cp etc/systemd/speed_log.service /etc/systemd/system/
 sudo cp etc/rsyslog.d/speed_log.conf /etc/rsyslog.d/
+sudo cp speed_log.py /usr/bin/
+sudo chmod +x /usr/bin/speed_log.py
 sudo systemctl daemon-reload
 sudo systemctl restart rsyslog.service
 sudo systemctl start speed_log.service
-sudo systemctl enable speed_log.service
+
+
+# Uncomment if you want to enable
+# sudo systemctl enable speed_log.service
